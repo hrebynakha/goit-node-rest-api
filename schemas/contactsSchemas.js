dfb1c88455b1fa584fdee66c1e2afb342666937c
Hrebynakha Anatolii <hrebynakha@gmail.com>
2025-06-09 21:43:09 +0300

    added validation for fields

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -1,15 +1,16 @@
 import Joi from "joi";
+const phoneRegex = /^\(\d{3}\)\s\d{3}-\d{4}$/;
 
 export const createContactSchema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().required(),
-  phone: Joi.string().required(),
+  name: Joi.string().min(3).max(30).required(),
+  email: Joi.string().email().required(),
+  phone: Joi.string().pattern(phoneRegex).required(),
 });
 
 export const updateContactSchema = Joi.object({
-  name: Joi.string().optional(),
-  email: Joi.string().optional(),
-  phone: Joi.string().optional(),
+  name: Joi.string().min(3).max(30).optional(),
+  email: Joi.string().email().optional(),
+  phone: Joi.string().pattern(phoneRegex).optional(),
 })
   .min(1)
   .message("Body must have at least one field");
